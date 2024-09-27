5f4865cec9e5207c296487f061dbd4f39c192b2c
jaad02 <juanantonioaznar2002@gmail.com>
2024-09-27 17:36:24 +0200

    push

---
+++ UD2/js/ej1/ej1.js
@@ -0,0 +1,20 @@
+let array = []
+let nombre = prompt("introduce el nombre")
+let apellido = prompt("introduce apellido")
+let suma = 0
+let multiplicacion = 1
+//cambio en el repo
+let num1 = prompt("introduce el numero 1")
+let num2 = prompt("introduce el numero 2" ) 
+let num3 = prompt("introduce el numero 3")  
+num1 = parseInt(num1)
+num2 = parseInt(num2)
+num3= parseInt(num3)
+
+suma =num1 + num2 + num3
+multiplicacion = num1*num2*num3
+let division = num1/num3
+
+
+console.log(`${nombre} ${apellido}`)
+
